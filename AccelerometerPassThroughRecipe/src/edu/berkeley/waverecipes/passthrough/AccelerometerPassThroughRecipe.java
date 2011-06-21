afd5491aeb9567fe5b33912c1a286b5a10e5319d
Philip Kuryloski <pjk25@cornell.edu>
2011-06-21 12:38:02 -0700

    Initial commit
    Added AccelerometerPassThroughRecipe project as
    created by 'android create project'

---
+++ AccelerometerPassThroughRecipe/src/edu/berkeley/waverecipes/passthrough/AccelerometerPassThroughRecipe.java
@@ -0,0 +1,15 @@
+package edu.berkeley.waverecipes.passthrough;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccelerometerPassThroughRecipe extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
